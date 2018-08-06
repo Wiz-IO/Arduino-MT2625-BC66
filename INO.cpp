9db34d368670a7585f79e0932a9ff341b7ecb2d8
Georgi Angelov <the.wizarda@gmail.com>
2018-08-06 22:11:48 +0300

    Update INO.cpp

--- INO.cpp
+++ INO.cpp
@@ -15,12 +15,12 @@ void timer_callback(TimerHandle_t xTimer) {
 /* TASK TEST */
 TaskHandle_t WizIO_TaskHandle;
 static portTASK_FUNCTION( WizIO_Task, pvParameters) {
-	timer = xTimerCreate("WizIO TIMER", 3000u, 1, (void *) 0, timer_callback);
+	timer = xTimerCreate("WizIO TIMER", 1000 /* 10 sec */, 1, NULL, timer_callback);
 	xTimerGenericCommand(timer, 1, 0, 0, 0);
 	TRACE_T("[API] xTimerGenericCommand( %u )", timer);
 	while (1) {
 		TRACE_T("[API] pcTaskGetTaskName() %s", pcTaskGetTaskName(WizIO_TaskHandle));
-		vTaskDelay(3000);
+		vTaskDelay(3000); // 30 sec
 	}
 }
 
@@ -29,14 +29,12 @@ void setup() {
 	S = "Arduino ";
 	S += "2018 Georgi Angelov";
 	TRACE_T("[INO] %s", S.c_str());
-	int r = xTaskGenericCreate(WizIO_Task, "WizIO TASK", (1024 * 4), (void *) NULL, TASK_PRIORITY_NORMAL, &WizIO_TaskHandle, NULL, NULL);
+	int r = xTaskGenericCreate(WizIO_Task, "WizIO TASK", 1024, NULL, TASK_PRIORITY_NORMAL, &WizIO_TaskHandle, NULL, NULL);
 	TRACE_T("[API] xTaskGenericCreate( %d )", r);
 	Serial.begin(115200);
 	Serial.println(S);
 	Serial.println("[INO] Quectel BC66 LTE NB-IoT");
 	pinMode(LED, OUTPUT);
-	//RIL_Test();
-	//bearer**.begin();
 }
 
 void loop() {
